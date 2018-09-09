27e72bb348647550691e596bb5758557064dcf3f
Brendan Harmon <brendan.harmon@gmail.com>
2018-09-09 17:22:00 -0500

    texts and markov chains

---
+++ scripts/markov-chains.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+SCRIPT:    markov-chains.py
+AUTHOR(S): Brendan Harmon <brendan.harmon@gmail.com>
+PURPOSE:   Open educational materials for a seminar on digital culture
+COPYRIGHT:  GNU General Public License v2. 
+"""
+
+# import libraries
+import markovify 
+
+# get raw text as string
+with open("../readings/mallarme-a-roll-of-the-dice.txt") as f:
+    text = f.read()
+
+# build the model
+text_model = markovify.NewlineText(text) #text_model = markovify.Text(text)
+
+# print three randomly-generated sentences of no more than 140 characters
+for i in range(3):
+    print text_model.make_short_sentence(140)
+
+print ""
+print ""
+print ""
+
+# get raw text as string
+with open("../readings/lovecraft-dunwich-horror.txt") as f:
+    text = f.read()
+
+# build the model
+text_model = markovify.Text(text)
+
+# print five randomly-generated sentences
+for i in range(5):
+    print text_model.make_sentence()
+
+
+print ""
+print ""
+print ""
+
+# get raw text as string
+with open("../readings/satyricon.txt") as f:
+    text = f.read()
+
+# build the model
+text_model = markovify.Text(text)
+
+# print five randomly-generated sentences
+for i in range(5):
+    print text_model.make_sentence()
+
+
+print ""
+print ""
+print ""
+
+
+# get raw text as string
+with open("../readings/lovecraft-dunwich-horror.txt") as f:
+    lovecraft = f.read()
+
+with open("../readings/satyricon.txt") as f:
+    satyricon = f.read()
+
+# build and combine the models
+lovecraft_model = markovify.Text(lovecraft)
+satyricon_model = markovify.Text(satyricon)
+model_synthesis = markovify.combine([lovecraft_model, satyricon_model], [ 1.5, 1 ])
+
+# print five randomly-generated sentences
+for i in range(5):
+    print model_synthesis.make_sentence()
